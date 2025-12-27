19b251b4a3afefad8f7090e8ff086321bd3674bb
xgkp <191536716@qq.com>
2025-12-27 22:00:12 +0800

    reverse use

--- project/urls.py
+++ project/urls.py
@@ -15,23 +15,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse
 from django.shortcuts import HttpResponse
 from book import views
 
 # def index(request):
 #     return HttpResponse("welcome to first project");
     
+def index(request):
+    # print(reverse("index"))
+    # /book/str/1
+    # print(reverse('book_str',kwargs={"book_id":1}))
+    # print(reverse('book_query_str') + '?id=1')
+    print(reverse('movie:movie_list'))
+    return HttpResponse('welcome to index')
+    
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index,name='index'),
     # path("s",index)
     # http://127.0.0.1:8000/book?id=3&name=666
-    path('book',views.get_book_query_string),
+    path('book',views.get_book_query_string,name='book_query_str'),
     # http://127.0.0.1:8000/book/1
     path('book/<int:book_id>',views.get_book_query_int),
     # 字符串可传空格
-    path('book/str/<str:book_id>',views.get_book_query_str),
+    path('book/str/<str:book_id>',views.get_book_query_str,name='book_str'),
     # slug可传-——
     path('book/slug/<slug:book_id>',views.get_book_query_slug,name='book_slug'),
     # path可传//
