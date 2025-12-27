8a1d3a6df56feadb5b272080dc9c86825186f423
xgkp <191536716@qq.com>
2025-12-27 23:49:03 +0800

    data to html

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ app_name = 'home'
 
 urlpatterns = [
     path('index',views.index,name='home_index'),
+    path('bd',views.bd,name='home_bd'),
+    path('info',views.info,name='home_info')
 ]
--- home/views.py
+++ home/views.py
@@ -4,5 +4,27 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     print('home/index')
-    return render(request,'index.html')
+    return render(request=request,template_name='index.html')
+
+def bd(request):
+    return render(request=request,template_name='bd.html')
+
+def info(request):
+    username = '课堂'
+    book = {"bookname":"shuihuzhuan","author":"施耐庵"}
+    class Person:
+        def  __init__(self,realname):
+            self.realname = realname + "123"
+    context = {
+        "username":username,
+        "book":book,
+        "books":[
+            {"bookname":"shuihuzhuan","author":"施耐庵"},
+            {"bookname":"xiyouji","author":"wuchengen"}
+        ],
+        "person":Person("真实姓名:")
+        }
+    
+    return render(request=request,template_name='info.html',context=context)
+    
     
